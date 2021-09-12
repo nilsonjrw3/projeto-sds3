8bb5ebd9a704e64c08d130d585c546534ad995f7
nilsonjrw3 <nilsonjuniorr2@gmail.com>
2021-09-12 01:34:56 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,8 @@
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
-import DataTable from "components/DataTable";
-import BarChart from "components/BarChart";
-import DonutChart from "components/DonutChart";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container" >
-        <h1 className="text-primary py-3">DashBoard Vendas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
-            <BarChart />
-          </div>
-
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as Vendas</h5>
-            <DonutChart />
-          </div>
-
-        </div>
-        <div className="text-primary py-5">
-          <h2>Relatório de Vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const DashBoard = () => {
+    return (
+        <>
+        <NavBar />
+        <div className="container" >
+          <h1 className="text-primary py-3">DashBoard Vendas</h1>
+          <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+              <BarChart />
+            </div>
+  
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Todas as Vendas</h5>
+              <DonutChart />
+            </div>
+  
+          </div>
+          <div className="text-primary py-5">
+            <h2>Relatório de Vendas</h2>
+          </div>
+          <DataTable />
+        </div>
+        <Footer />
+      </>
+  
+    );
+  }
+  
+  export default DashBoard;
